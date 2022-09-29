0cfc8223f2960e9d676549ab2522930bdc0a797f
Hi82006 <hi82006@gmail.com>
2022-09-30 00:33:56 +0300

    commit

---
+++ js/calc.js
@@ -0,0 +1,10 @@
+let a = prompt('калькулятор', 'введите число');
+let math = prompt('калькулятор', 'введите + - * /');
+let b = prompt('калькулятор', 'введите второе число');
+// let firstValue = parseFloat(a);
+// let secondValue = parseFloat(b);
+let str = a+math+b;
+alert(eval(str))
+
+
+
