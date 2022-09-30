cc778bbe43155d70fc2ab8b1f57d42bdec712a0c
Hi82006 <hi82006@gmail.com>
2022-09-30 13:10:12 +0300

    commit

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,14 @@
 let a = prompt('калькулятор', 'введите число');
 let math = prompt('калькулятор', 'введите + - * /');
 let b = prompt('калькулятор', 'введите второе число');
-// let firstValue = parseFloat(a);
-// let secondValue = parseFloat(b);
 let str = a+math+b;
 alert(eval(str))
 
+let firstVar = prompt('User name: ', 'введите имя');
+let secondVar = prompt('User last name:', 'введите фамилию');
+let userDataInfo = 'User name: ' + firstVar + ',  User last name: ' + secondVar;
+document.writeln(userDataInfo);
 
+let lang = prompt('input lang: uk or ru');
+console.log(lang == 'uk');
 
